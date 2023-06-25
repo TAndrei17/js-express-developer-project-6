59847368a7e7335ff3712773cec610a18cd753d3
Andrei Trunkin <inboxavt@gmail.com>
2023-06-25 21:31:31 +0200

    Step 1. Production preparation

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 // @ts-check
-import fastify from 'fastify';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
-const app = fastify({
-  exposeHeadRoutes: false,
-  logger: { target: 'pino-pretty' },
+const fastify = require('fastify')({
+  // @ts-ignore
+  logger: { prettyPrint: true },
 });
 
-export default app;
+export default fastify;
