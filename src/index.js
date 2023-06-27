713e99491555a96c2eeaf5b350e13c33f6616d5a
Andrei Trunkin <inboxavt@gmail.com>
2023-06-27 23:34:47 +0200

    Step 1. Production preparation

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 // @ts-check
-
+import 'dotenv/config';
+import fastify from 'fastify';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
-const fastify = require('fastify')({
+require('dotenv').config();
+
+const app = fastify({
+  exposeHeadRoutes: false,
   // @ts-ignore
-  logger: { prettyPrint: true },
+  logger: { target: 'pino-pretty' },
 });
 
-export default fastify;
+export default app;
